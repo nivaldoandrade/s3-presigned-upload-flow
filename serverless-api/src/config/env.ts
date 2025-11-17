b256eda69a9efb8b7b17f2acef57d235d65e3ff5
nivaldoandrade <nivaldoandradef@gmail.com>
2025-11-17 16:24:55 -0300

    feat: integrate s3 presignedurl flow for file upload

---
+++ serverless-api/src/config/env.ts
@@ -0,0 +1,4 @@
+
+export const env = {
+  FILE_BUCKET_NAME: process.env.FILE_BUCKET_NAME!,
+};
--- serverless-api/src/functions/createImage.ts
+++ serverless-api/src/functions/createImage.ts
@@ -1,5 +1,9 @@
+import { PutObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { APIGatewayProxyEventV2 } from 'aws-lambda';
 import * as z from 'zod/mini';
+import { s3Client } from '../clients/s3Client';
+import { env } from '../config/env';
 import { bodyParser } from '../utils/bodyParser';
 import { lambdaHttpResponse } from '../utils/lambdaHttpResponse';
 
@@ -28,7 +32,16 @@ export async function handler(event: APIGatewayProxyEventV2) {
 
   const fileKey = `${Date.now()}_${filename}`;
 
+  const commmand = new PutObjectCommand({
+    Bucket: env.FILE_BUCKET_NAME,
+    Key: fileKey,
+  });
+
+  const preSignedUrl = await getSignedUrl(s3Client, commmand, {
+    expiresIn: 60, //1min
+  });
+
   return lambdaHttpResponse(200, {
-    message: fileKey,
+    message: preSignedUrl,
   });
 }
