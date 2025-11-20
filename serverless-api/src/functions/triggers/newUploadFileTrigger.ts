d656e5f9d08705796f81bdb01c93f691da0c70c3
nivaldoandrade <nivaldoandradef@gmail.com>
2025-11-20 13:19:01 -0300

    feat(serverless-api): add s3 trigger to update file status after upload

---
+++ serverless-api/src/functions/triggers/newUploadFileTrigger.ts
@@ -0,0 +1,27 @@
+import { UpdateCommand } from '@aws-sdk/lib-dynamodb';
+import { S3Event } from 'aws-lambda';
+import { dynamoClient } from '../../clients/dynamoClient';
+import { env } from '../../config/env';
+
+export async function handler(event: S3Event) {
+  const id = decodeURIComponent(
+    event.Records[0].s3.object.key.replace(/\+/g, ' '),
+  );
+
+  const updateCommand = new UpdateCommand({
+    TableName: env.TABLE_NAME,
+    Key: {
+      id,
+    },
+    UpdateExpression: 'SET #status = :status',
+    ExpressionAttributeNames: {
+      '#status': 'status',
+    },
+    ExpressionAttributeValues: {
+      ':status': 'COMPLETED',
+    },
+  });
+
+  await dynamoClient.send(updateCommand);
+
+}
